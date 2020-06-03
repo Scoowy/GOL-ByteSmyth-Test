bd0306fdaaf6b7b88ce5fac5d4447250dc6f21bd
Scoowy <gahonajuanjo@gmail.com>
2020-06-03 12:21:56 -0500

    #2 * Display the simulation with JavaFX close #2

--- src/main/java/org/codecat/Simulation.java
+++ src/main/java/org/codecat/Simulation.java
@@ -101,9 +101,6 @@ public class Simulation {
 
     public static void main(String[] args) {
         Simulation simulation = new Simulation(8, 5);
-        simulation.setAlive(2, 2);
-        simulation.setAlive(3, 2);
-        simulation.setAlive(4, 2);
 
         simulation.printBoard();
 
