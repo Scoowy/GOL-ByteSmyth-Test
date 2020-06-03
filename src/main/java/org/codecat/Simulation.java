b2bff8b0f45672272f937c5e80a0b2562c8da450
Scoowy <gahonajuanjo@gmail.com>
2020-06-03 13:43:13 -0500

    #3 * Implement board editing close #3

--- src/main/java/org/codecat/Simulation.java
+++ src/main/java/org/codecat/Simulation.java
@@ -37,11 +37,21 @@ public class Simulation {
     }
 
     public void setAlive(int x, int y) {
-        this.board[x][y] = 1;
+        this.setState(x, y, 1);
     }
 
     public void setDead(int x, int y) {
-        this.board[x][y] = 0;
+        this.setState(x, y, 0);
+    }
+
+    public void setState(int x, int y, int state) {
+        if (x < 0 || x >= width) {
+            return;
+        }
+        if (y < 0 || y >= height) {
+            return;
+        }
+        this.board[x][y] = state;
     }
 
     public int countAliveNeighbours(int x, int y) {
@@ -97,16 +107,5 @@ public class Simulation {
         }
 
         this.board = newBoard;
-    }
-
-    public static void main(String[] args) {
-        Simulation simulation = new Simulation(8, 5);
-
-        simulation.printBoard();
-
-        simulation.step();
-        simulation.printBoard();
-        simulation.step();
-        simulation.printBoard();
     }
 }
